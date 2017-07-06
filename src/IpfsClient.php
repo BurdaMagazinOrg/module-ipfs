14a6f2bf654c89d78a3c30b576590c5599408813
Daniel Bosen <daniel.bosen@burda.com>
2017-07-06 10:04:07 +0200

    Fully document the IPFS interface and class

--- src/IpfsClient.php
+++ src/IpfsClient.php
@@ -3,51 +3,78 @@
 namespace Drupal\ipfs;
 
 /**
- *
+ * Implementation of an IPFS client.
  */
 class IpfsClient implements IpfsClientInterface {
-  private $apiVersion;
-  private $gatewayIP;
+
+  /**
+   * The address of the ipfs gateway, defaults to localhost.
+   *
+   * @var string
+   */
+  private $gatewayAddress;
+
+  /**
+   * The readonly port of the gateway, defaults to 8080.
+   *
+   * @var int
+   */
   private $gatewayPort;
+
+  /**
+   * The API port of the gateway, defaults to 5001.
+   *
+   * @var int
+   */
   private $gatewayApiPort;
+
+  /**
+   * The API version to use, currently defaults to v0.
+   *
+   * @var string
+   */
+  private $gatewayApiVersion;
 
   /**
    * IpfsClient constructor.
    *
-   * @param string $ip
-   * @param int $port
-   * @param int $apiPort
-   * @param string $apiVersion
+   * @param string $gateway_address
+   *   The address of the ipfs gateway.
+   * @param int $gateway_port
+   *   The readonly port of the gateway.
+   * @param int $gateway_api_port
+   *   The API port of the gateway.
+   * @param string $gateway_api_version
+   *   The API version to use.
    */
-  public function __construct($ip = "localhost", $port = 8080, $apiPort = 5001, $apiVersion = 'v0') {
-    $this->gatewayIP      = $ip;
-    $this->gatewayPort    = $port;
-    $this->gatewayApiPort = $apiPort;
-    $this->apiVersion     = $apiVersion;
+  public function __construct($gateway_address = "localhost", $gateway_port = 8080, $gateway_api_port = 5001, $gateway_api_version = 'v0') {
+    $this->gatewayAddress    = $gateway_address;
+    $this->gatewayPort       = $gateway_port;
+    $this->gatewayApiPort    = $gateway_api_port;
+    $this->gatewayApiVersion = $gateway_api_version;
   }
 
   /**
-   *
+   * {@inheritdoc}
    */
   public function cat($hash) {
-    $url = 'http://' . $this->gatewayIP . ':' . $this->gatewayPort . '/ipfs/' . $hash;
+    $url = 'http://' . $this->gatewayAddress . ':' . $this->gatewayPort . '/ipfs/' . $hash;
     return $this->curl($url);
   }
 
   /**
-   *
+   * {@inheritdoc}
    */
   public function addFiles(array $files, $pin = TRUE) {
     $query_vars = [
       'pin' => ($pin ? 'true' : 'false'),
     ];
     $response = $this->postFile($files, $query_vars);
-    return $response['Hash'];
+    return $response;
   }
 
   /**
-   * @param $content
-   * @return string
+   * {@inheritdoc}
    */
   public function addData($data) {
     $content = ['data' => $data];
@@ -56,7 +83,7 @@ class IpfsClient implements IpfsClientInterface {
   }
 
   /**
-   *
+   * {@inheritdoc}
    */
   public function ls($hash) {
     $response = $this->apiCall("ls/$hash");
@@ -64,7 +91,7 @@ class IpfsClient implements IpfsClientInterface {
   }
 
   /**
-   *
+   * {@inheritdoc}
    */
   public function size($hash) {
     $response = $this->apiCall("object/stat/$hash");
@@ -72,21 +99,21 @@ class IpfsClient implements IpfsClientInterface {
   }
 
   /**
-   *
+   * {@inheritdoc}
    */
   public function pinAdd($hash) {
     return $this->apiCall("add/$hash");
   }
 
   /**
-   *
+   * {@inheritdoc}
    */
   public function pinRemove($hash) {
     return $this->apiCall("rm/$hash");
   }
 
   /**
-   *
+   * {@inheritdoc}
    */
   public function version() {
     $response = $this->apiCall('version');
@@ -94,44 +121,71 @@ class IpfsClient implements IpfsClientInterface {
   }
 
   /**
+   * Creates gateway "add" requests for one or multiple files to IPFS.
    *
-   */
-  public function get(array $query_vars = []) {
-    $query_string = $query_vars ? '?' . http_build_query($query_vars) : '';
-    return $this->apiCall("get" . $query_string);
-  }
-
-  /**
+   * @param array $files
+   *   Array of local filepaths to add.
+   * @param array $query_vars
+   *   Array of additional query variables, that should be added to the request.
+   *   The variables have to key-value pairs, that can be used by
+   *   http_build_query.
    *
+   * @return array
+   *   Array of responses for all files.
+   *
+   * @TODO: The connection between local files and the response should be clearer.
    */
-  protected function postFile(array $files, array $query_vars = [], $expect_multiple = FALSE) {
+  protected function postFile(array $files, array $query_vars = []) {
     $query = $query_vars ? '?' . http_build_query($query_vars) : '';
     $content = ['files' => $files];
     $response = $this->apiCall("add" . $query, $content);
-    if ($expect_multiple) {
-      $data = [];
-      foreach (explode("\n", $response) as $json_data_line) {
-        if (strlen(trim($json_data_line))) {
-          $data[] = $json_data_line;
-        }
+
+    $data = [];
+    foreach (explode("\n", $response) as $json_data_line) {
+      if (strlen(trim($json_data_line))) {
+        $data[] = $json_data_line;
       }
     }
-    else {
-      $data = $response;
-    }
+
     return $data;
   }
 
   /**
+   * Call to the API.
    *
+   * @param string $query
+   *   The query string to send to the API.
+   * @param array $content
+   *   The conrent array. This can have two different keys.
+   *   Either 'files', which is an array of file paths to add to IPFS,
+   *   or 'data', which is a blob of data, that should be stored in a file on
+   *   IPFS.
+   *   TODO: refactor to be more clear and to be able to upload multiple blobs.
+   *
+   * @return mixed
+   *   The response.
    */
   protected function apiCall($query, array $content = []) {
-    $url = 'http://' . $this->gatewayIP . ':' . $this->gatewayApiPort . '/api/' . $this->apiVersion . '/' . $query;
+    $url = 'http://' . $this->gatewayAddress . ':' . $this->gatewayApiPort . '/api/' . $this->gatewayApiVersion . '/' . $query;
     return $this->curl($url, $content);
   }
 
   /**
+   * Create and handle the http call to the gateway.
    *
+   * @param string $url
+   *   The endpoint to call.
+   * @param array $content
+   *   The conrent array. This can have two different keys.
+   *   Either 'files', which is an array of file paths to add to IPFS,
+   *   or 'data', which is a blob of data, that should be stored in a file on
+   *   IPFS.
+   *   TODO: refactor to be more clear and to be able to upload multiple blobs.
+   *
+   * @return mixed
+   *   The response to the call.
+   *
+   * @throws \Exception
    */
   protected function curl($url, array $content = []) {
     $ch = curl_init();
--- src/IpfsClientInterface.php
+++ src/IpfsClientInterface.php
@@ -3,61 +3,94 @@
 namespace Drupal\ipfs;
 
 /**
- *
+ * The interface for an IPFS client.
  */
 interface IpfsClientInterface {
 
   /**
+   * Retrieve the contents of a file.
    *
+   * @param string $hash
+   *   The hash of the file.
+   *
+   * @return string
+   *   The content of the file.
    */
   public function cat($hash);
 
   /**
-   * Adds a file directly to IPFS.
+   * Adds files to IPFS.
    *
    * @param array $files
-   *   A filepath to upload.
+   *   Array of local filepath to upload.
    * @param bool $pin
-   *   Pin the file (true)
+   *   Whether to pin the file or not.
    *
    * @return string
-   *   The content hash.
+   *   The gateway response.
    */
   public function addFiles(array $files, $pin);
 
   /**
-   * @param $content
-   * @return mixed
+   * Add a data blob to IPFS to create a new file.
+   *
+   * @param string $content
+   *   The data that gets put into the file.
+   *
+   * @return string
+   *   The hash of the created file.
    */
   public function addData($content);
 
   /**
+   * List directory contents.
    *
+   * @param string $hash
+   *   The hash of the path to list contents from.
+   *
+   * @return array
+   *   The content of the given directory path.
    */
   public function ls($hash);
 
   /**
+   * Get the size of an object.
    *
+   * @param string $hash
+   *   The hash of the object.
+   *
+   * @return string
+   *   The size of the object.
    */
   public function size($hash);
 
   /**
+   * Pin an object.
    *
+   * @param string $hash
+   *   The hash of the object.
+   *
+   * @return array
+   *   The gateway response.
    */
   public function pinAdd($hash);
 
   /**
+   * Unpin an object.
    *
+   * @param string $hash
+   *   The hash of the object.
+   *
+   * @return array
+   *   The gateway response.
    */
   public function pinRemove($hash);
 
   /**
+   * Get the version of the gateway.
    *
-   */
-  public function get(array $query_vars);
-
-  /**
-   *
+   * @return string
+   *   The version of the gateway.
    */
   public function version();
 
