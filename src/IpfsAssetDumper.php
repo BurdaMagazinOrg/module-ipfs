b92b8ac61bb0579209acf243b4559b3451b2231d
Mladen Todorovic <mladen.todorovic@burda.com>
2017-07-07 13:19:31 +0200

    Loading of Assests from IPFS

---
+++ src/IpfsAssetDumper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\ipfs;
+
+use Drupal\Core\Asset\AssetDumper;
+
+/**
+ * Extension of asset dumper class to store generated files to IPFS.
+ *
+ * @package Drupal\ipfs
+ */
+class IpfsAssetDumper extends AssetDumper {
+
+  /**
+   * IPFS Handler service.
+   *
+   * @var \Drupal\ipfs\IpfsHandler
+   */
+  protected $ipfsHandler;
+
+  /**
+   * IpfsAssetDumper constructor.
+   */
+  public function __construct(IpfsHandler $ipfsHandler) {
+    $this->ipfsHandler = $ipfsHandler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function dump($data, $file_extension) {
+    $uri = parent::dump($data, $file_extension);
+
+    $uid = preg_replace('/^public:\//', '', $uri);
+    $type = trim(substr($uid, 0, 4), '/');
+
+    $this->ipfsHandler->add($uid, $type, $data);
+
+    file_put_contents('test.txt', $uri . PHP_EOL, FILE_APPEND);
+
+    return $uri;
+  }
+
+}
